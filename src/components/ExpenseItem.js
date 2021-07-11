93ecf1ac16d2ce5d88fcbb92d2e0bdb3141ea248
emmanuel <emmanuelpallares50@gmail.com>
2021-07-10 23:58:27 -0500

    fixed the bug that cause dont appear the mount in the list and add a filter for the expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -13,13 +13,11 @@ const ExpenseItem = (props) => {
   };
 
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
       {props.name}
       <div>
-        <span className="badge badge-primary badge-pill mr-3">
-          ${props.cost}
-        </span>
-        <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+        <span>$ {props.cost}</span>
+        <TiDelete size="1.5em" onClick={handleDeleteExpense} />
       </div>
     </li>
   );
